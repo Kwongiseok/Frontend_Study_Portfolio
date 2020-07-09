06c2a4a811fdd1e329ef31e2820837049a3b1a27
Kwongiseok <ri2377@naver.com>
2020-07-09 14:44:08 +0900

    Javascript : Home Contact Btn

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const navbarHeight = navbar.getBoundingClientRect().height;
 
 // Make navbar transparent when it is on the top
 document.addEventListener("scroll", () => {
-  if (window.scrollY > navbarHeight) {
+  if (window.scrollY + 35 > navbarHeight) {
     navbar.classList.add("navbar--dark");
   } else {
     navbar.classList.remove("navbar--dark");
@@ -20,7 +20,21 @@ navbarMenu.addEventListener("click", (event) => {
   if (link == null) {
     return;
   }
-
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({ behavior: "smooth" });
+  scrollIntoView(link);
 });
+
+// Handle Contact
+const contactBtn = document.querySelector(".home__contact");
+contactBtn.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  scrollIntoView(link);
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
+}
